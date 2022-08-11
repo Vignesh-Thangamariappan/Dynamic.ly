8bf0bb464b13fe4b16053f59e59817bfb380e29f
Vignesh <vignesh.mariappan@anywhere.co>
2022-08-11 18:14:10 +0530

    Initial Commit

---
+++ Dynamic.ly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamic.ly
+//
+//  Created by Vignesh on 11/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
