9d85b7bdb660cf41a69dfe6a577c33bba621fec3
Vignesh <vignesh.mariappan@anywhere.co>
2023-01-21 22:20:52 +0530

    Fix: Resolved conflicts

--- Dynamic.ly/ViewController.swift
+++ Dynamic.ly/ViewController.swift
@@ -8,69 +8,6 @@
 import UIKit
 import CustomViewPresenter
 import SwiftDate
-import NavigationKit
-
-class ViewController: UIViewController, NavigationEnabled, SearchEnabled {
-    var searchController: UISearchController = UISearchController()
-    
-    lazy var leftBarButtons: [NavigationKit.BarButtonItem] = [
-        .init(identifier: "Settings.title", mode: .image((UIImage(named: "Settings")!.withTintColor(.black).withRenderingMode(.alwaysOriginal))), target: nil, action: nil)
-    ]
-    
-    lazy var rightBarButtons: [NavigationKit.BarButtonItem] = [
-        .closeButton(target: self, action: #selector(didTapCloseButton)),
-        .backButton(target: self, action: #selector(actionForBackButton))
-    ]
-    
-    var prefersLargeTitle: Bool = true
-    
-    var showsSearchBar: Bool = true
-    
-    var hidesNavBarWhileSearch: Bool = true
-    
-    func didSearch(for text: String?) {
-        print("Searched for \(text)")
-    }
-    
-    @objc
-    func updateSearchResults(for searchController: UISearchController) {
-        didSearch(for: searchController.searchBar.text)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Header Title", getHeaderTitle(forDate: Date()))
-        setupNavigation()
-        setupSearch()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.set(title: "Older")
-            self.set(rightBarButtons: [])
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-            self.set(title: "Newbie")
-            self.set(rightBarButtons: [
-                .closeButton(target: self, action: #selector(self.didTapCloseButton)),
-                .backButton(target: self, action: #selector(self.actionForBackButton))
-            ])
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
-            self.set(title: "Work Mode")
-            self.set(rightBarButtons: [])
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-//        let newVC = NewVC()
-//        self.interactivelyPresent(newVC, animated: true) {
-//        }
-    }
-}
 
 class NewVC: UIViewController, CustomViewPresentable {
     func didChangeToFullScreen() {
